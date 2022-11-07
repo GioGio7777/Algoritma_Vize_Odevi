5f98dd6f603b925a80cce68f0a856c3cd8899414
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2022-11-07 23:23:36 +0300

    Algo

---
+++ src/Folk_Dance/FolkDance.java
@@ -0,0 +1,63 @@
+package Folk_Dance;
+
+public class FolkDance {
+
+    private static void swapArray(int[] array,int i,int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static int FolkPartition(int[] arr, int low, int high) {
+        int pivot = low;
+        int indexer = high;
+        int multiply = -1;
+        int x = 0;
+
+        for (int i = 0; i <(high - low); i++) {
+            if(x == 0){
+                if(arr[pivot]>arr[indexer]){
+                    swapArray(arr,pivot,indexer);
+                    int temp = pivot;
+                    pivot = indexer;
+                    indexer = temp;
+                    x=1;
+                    multiply = 1;
+                }
+            }
+
+            if(x == 1){
+                if(arr[pivot]<arr[indexer]){
+                    swapArray(arr,pivot,indexer);
+                    int temp = pivot;
+                    pivot = indexer;
+                    indexer = temp;
+                    x=0;
+                    multiply = -1;
+                }
+
+            }
+
+            indexer += multiply;
+
+        }
+
+        return pivot;
+    }
+
+    public static void FolkSort(int[] arr,int low,int high){
+        if(low<high){
+            int index = FolkPartition(arr,low,high);
+            FolkSort(arr,low,index - 1);
+            FolkSort(arr,index + 1, high);
+
+        }
+    }
+
+    public static void writeArray(int[] sortedArray){
+        for (int value:sortedArray) {
+            System.out.print(value+",");
+        }
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+import Folk_Dance.FolkDance;
+
+public class main {
+    public static void main(String[] args) {
+        int[] array = {3,0,1,8,7,2,5,4,9,6};
+        int[] basic = {4,3,2,1};
+        FolkDance.FolkSort(array,0,array.length-1);
+
+    }
+}
