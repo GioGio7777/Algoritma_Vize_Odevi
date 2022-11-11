aa9cc78319d1041d6c04b46903d5ecbbd757f2ca
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2022-11-11 20:45:36 +0300

    .

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 import Akdeniz_Missing.Missing;
 import Folk_Dance.FolkDance;
 import Generate_Array.Generate;
+import Shift.Shift;
 
 public class main {
     public static void main(String[] args) {
@@ -8,12 +9,20 @@ public class main {
         int[] array2 =Generate.generateArray(3);
 
         //System.out.println("\n"+FolkSortHw2(array2)/1000000 + "ms");
-        int [] newArray = {12,7,10,4,5,3,11,1,6,8,2};
+        //int [] newArray = {12,7,10,4,5,3,11,1,6,8,2};
 
-        Missing.MissingElement(newArray,0, newArray.length-1);
+        //Missing.MissingElement(newArray,0, newArray.length-1);
 
-        FolkDance.writeArray(newArray);
-    }
+        //FolkDance.writeArray(newArray);
+        int[] A = {9,12,17,21,33,41};
+        int k = 4;
+        Shift.rightRotate(A,k);
+        FolkDance.writeArray(A);
+
+        System.out.println();
+        Shift.insertationSort(A);
+        FolkDance.writeArray(A);
+     }
 
     public static void MissingHw1(int[] arr){
 
